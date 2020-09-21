7224f19ae46d787fb44ef5ca2c4f6438cbc30b6c
zxy <1721276667@qq.com>
2020-09-21 13:07:47 +0800

    修改了model的class类

--- src/main/java/com/jxd/model/Class.java
+++ src/main/java/com/jxd/model/Class.java
@@ -8,4 +8,28 @@ package com.jxd.model;
 public class Class {
     private int classId;
     private String className;
+
+    public Class(int classId, String className) {
+        this.classId = classId;
+        this.className = className;
+    }
+
+    public Class() {
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
 }
