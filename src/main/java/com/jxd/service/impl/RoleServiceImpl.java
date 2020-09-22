b47c738d127fb5d69756aa01533ee2e4d0fdfd66
zxy <1721276667@qq.com>
2020-09-22 10:13:55 +0800

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	.idea/FollowSystem.iml

--- src/main/java/com/jxd/service/impl/RoleServiceImpl.java
+++ src/main/java/com/jxd/service/impl/RoleServiceImpl.java
@@ -2,6 +2,8 @@ package com.jxd.service.impl;
 
 import com.jxd.model.Role;
 import com.jxd.service.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -10,9 +12,12 @@ import java.util.List;
  * @description Todo
  * @date 2020-09-22 10:09
  */
+@Service
 public class RoleServiceImpl implements IRoleService {
+    @Autowired
+    private IRoleService roleService;
     @Override
     public List<Role> getRoleByUserId(int UserId) {
-        return null;
+        return roleService.getRoleByUserId(UserId);
     }
 }
